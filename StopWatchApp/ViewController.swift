c1ee342c19963f37c1d93d474d29147024a40d10
djamaldev <seurdjimo@gmail.com>
2018-09-07 00:55:07 +0100

    Initial Commit

---
+++ StopWatchApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StopWatchApp
+//
+//  Created by mr Yacine on 07/09/2018.
+//  Copyright © 2018 mr Yacine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
